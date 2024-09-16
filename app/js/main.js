109b7a37c4d100e5236b4c4a8a945b98fcb27658
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2024-09-16 21:37:24 +0300

    created a sprite-SVG.Slider for the chapter hero and added chapter partners

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,12 @@
-var mixer = mixitup('.card');
+$(document).ready(function () {
+    $('.hero__slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+
+    });
+    var mixer = mixitup('.works__card-list');
+})
+
