fb3c608b0e7a59343361755cfe16d48a444227a2
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2024-10-14 20:56:40 +0300

    made an adaptive for the entire site

--- app/js/main.js
+++ app/js/main.js
@@ -2,24 +2,34 @@ $(document).ready(function () {
     $('.hero__slider').slick({
         arrows: false,
         fade: true,
-        autoplay: true,
-        autoplaySpeed: 1500,
+        // autoplay: true,
+        // autoplaySpeed: 1500,
         infinite: true,
     });
-
     const mixer = mixitup('.works__card-list');
 });
 
 (() => { //А для чого потрібно, щоб оцей весь код одразу виконувався, не дивлячись на те, чи завантажили DOM елементи чи ні ?
+    const menuBtnRef = document.querySelectorAll("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+
+    menuBtnRef.forEach(function (item) {
+        item.addEventListener("click", () => {
+            const expanded =
+                item.getAttribute("aria-expanded") === "true" || false;
+            item.classList.toggle("is-open");
+            item.setAttribute("aria-expanded", !expanded);
+            mobileMenuRef.classList.toggle("is-open");
+        });
+    })
+
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
         modal: document.querySelector("[data-modal]"),
     };
-
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
-
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
     }
