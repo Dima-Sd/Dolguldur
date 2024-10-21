537eda2a0ad949b263e30c5ceff4d61e35416827
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2024-10-21 20:00:29 +0300

    Made changes to the entire website for responsive layout.

--- app/js/main.js
+++ app/js/main.js
@@ -2,25 +2,50 @@ $(document).ready(function () {
     $('.hero__slider').slick({
         arrows: false,
         fade: true,
-        // autoplay: true,
-        // autoplaySpeed: 1500,
+        autoplay: true,
+        autoplaySpeed: 1500,
         infinite: true,
     });
     const mixer = mixitup('.works__card-list');
 });
 
-(() => { //А для чого потрібно, щоб оцей весь код одразу виконувався, не дивлячись на те, чи завантажили DOM елементи чи ні ?
-    const menuBtnRef = document.querySelectorAll("[data-menu-button]");
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+        mobileMenuRef.classList.toggle("is-open");
+    })
 
-    menuBtnRef.forEach(function (item) {
-        item.addEventListener("click", () => {
-            const expanded =
-                item.getAttribute("aria-expanded") === "true" || false;
-            item.classList.toggle("is-open");
-            item.setAttribute("aria-expanded", !expanded);
-            mobileMenuRef.classList.toggle("is-open");
-        });
+    const header = document.querySelector('[data-header]');
+    window = window.addEventListener('scroll', function () {
+        header.classList.add('active')
+        let scrollPosition = this.scrollY
+        if (scrollPosition > 0) {
+            header.classList.add('active')
+        } else {
+            header.classList.remove('active')
+        }
+    });
+
+    const body = document.body;
+    const buttonMenu = document.querySelector('[data-menu-button]')
+    const buttonModalOpen = document.querySelector('[data-modal-open]')
+    const buttonModalClose = document.querySelector('[data-modal-close]')
+
+    buttonMenu.addEventListener('click', function () {
+        body.classList.toggle('no-scroll')
+    })
+
+    buttonModalOpen.addEventListener('click', function () {
+        body.classList.add('no-scroll')
+    })
+
+    buttonModalClose.addEventListener('click', function () {
+        body.classList.remove('no-scroll')
     })
 
     const refs = {
